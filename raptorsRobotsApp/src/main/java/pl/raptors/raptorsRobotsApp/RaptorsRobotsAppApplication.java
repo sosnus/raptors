39b93437fb1a6cb1edd36480bd1076e5f55c8469
Mariusz929 <mariusz.bezulski@gmail.com>
2019-11-20 15:44:14 +0100

    initial commit

---
+++ raptorsRobotsApp/src/main/java/pl/raptors/raptorsRobotsApp/RaptorsRobotsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.raptors.raptorsRobotsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaptorsRobotsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaptorsRobotsAppApplication.class, args);
+	}
+
+}
---
+++ raptorsRobotsApp/src/test/java/pl/raptors/raptorsRobotsApp/RaptorsRobotsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.raptors.raptorsRobotsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaptorsRobotsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
