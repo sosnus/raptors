8231e33dc979d03f23f22de8b4250d27b22919ef
mgrzejszczak <marek.grzejszczak@comarch.pl>
2019-12-06 10:57:46 +0100

    Markers setup icons

--- angularclient/src/app/components/map/map.component.ts
+++ angularclient/src/app/components/map/map.component.ts
@@ -13,54 +13,73 @@ export class MapComponent implements OnInit {
 
   //Example data
   //Leaflet accepts coordinates in [y,x]
-  private robotPosXY = [992, 992];
+  private robotPosXY = [19.84/2, 19.84/2];
   private robotMarkers = [];
-  private imageResolution = 1984;
+  private imageResolution = 1984; //temp, should be 1;
 
   private map;
+  private imageURL = './assets/maps/sim_map.jpg';
 
   constructor() {
   }
 
   ngOnInit() {
     // this.mapService.getMap('1').subscribe(
-    //   data => this.convertToMap(data)
+    //   data => {
+    //     this.imageURL = this.parseToJpeg(data);
+    //     this.getImageSize(this.imageURL).then(
+    //       (imageSize: number[]) => {
+    //         this.imageResolution = imageSize[0];
+    //
+    //       }
+    //     );
+    //   }
     // );
     this.initMap();
     const robotsArray = [this.robotPosXY];
     this.createRobotMarkers(robotsArray, 0.01);
-    setTimeout(() => this.updateRobotMarkerPositions([[100, 992]], 0.01), 3000);
+    //setTimeout(() => this.updateRobotMarkerPositions([[100, 992]], 0.01), 3000);
   }
 
-  private convertToMap(image: any): void {
+  private parseToJpeg(image: any): string {
     const base64Data = btoa(image);
-    const imageURL = 'data:image/jpg;base64,' + base64Data;
+    return 'data:image/jpg;base64,' + base64Data;
+  }
+
+  private getImageSize(fileURL: string): Promise<number[]> {
+    const img = new Image;
+    img.src = fileURL;
+    return new Promise(img.onload = () => {
+      return [img.width, img.height];
+    });
   }
 
   private initMap(): void {
-    const imageUrl = './assets/maps/sim_map.jpg';
     const imageBounds = [[0, 0], [800, 800]];
     this.map = L.map('map', {
       crs: L.CRS.Simple
     });
-    L.imageOverlay(imageUrl, imageBounds).addTo(this.map);
+    L.imageOverlay(this.imageURL, imageBounds).addTo(this.map);
     this.map.fitBounds(imageBounds);
   }
 
   private createRobotMarkers(robots: number[][], resolution: number) {
     robots.forEach(robot => {
-      const position = [robot[1] * (800 / this.imageResolution), robot[0] * (800 / this.imageResolution)];
+      const markerIcon = L.icon({iconUrl: '/assets/icons/drone.png', iconSize: [35,35]});
+      const position = [
+        robot[1] * (1 / resolution) * (800 / this.imageResolution),
+        robot[0] * (1 / resolution) * (800 / this.imageResolution)];
       this.robotMarkers.push(
-        L.marker(position, {
-          rotationAngle: 45
-        }).addTo(this.map)
+        L.marker(position, {icon: markerIcon}).addTo(this.map)
       )
     })
   }
 
   private updateRobotMarkerPositions(robots: number[][], resolution: number) {
     for (let i = 0; i < robots.length; i++) {
-      const position = [robots[i][1] * (800 / 1984), robots[i][0] * (800 / 1984)];
+      const position = [
+        robots[i][1] * (1 / resolution) * (800 / this.imageResolution),
+        robots[i][0] * (1 / resolution) * (800 / this.imageResolution)];
       this.robotMarkers[i].setLatLng(position);
     }
   }
