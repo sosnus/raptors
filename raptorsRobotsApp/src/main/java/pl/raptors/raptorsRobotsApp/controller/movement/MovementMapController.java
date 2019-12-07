449971b33d01d2fe1baed1c9d09972c660869517
ita-11 <ita-11@sr.dmcs.pl>
2019-12-07 20:37:52 +0100

    Added PGM to JPG conversion

--- raptorsRobotsApp/src/main/java/pl/raptors/raptorsRobotsApp/controller/movement/MovementMapController.java
+++ raptorsRobotsApp/src/main/java/pl/raptors/raptorsRobotsApp/controller/movement/MovementMapController.java
@@ -7,30 +7,36 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.raptors.raptorsRobotsApp.domain.movement.MovementMap;
 import pl.raptors.raptorsRobotsApp.service.movement.MovementMapService;
+import pl.raptors.raptorsRobotsApp.service.movement.PGMIO;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
 
 @Controller
 @RequestMapping(value = "/movement/maps", method = {RequestMethod.GET, RequestMethod.POST})
 public class MovementMapController {
+
     @Autowired
     MovementMapService movementMapService;
 
     @PostMapping("/add")
     //@PostMapping("/maps/add")
     public String addMovementMap(@RequestParam("name") String name, @RequestParam("mapImage") MultipartFile mapImage, Model model) throws IOException {
-        String id = movementMapService.addMovementMap(name,mapImage);
-        return "redirect:/movement/maps/"+id;
-       /* return "redirect:/maps/"+id;*/
+        String id = movementMapService.addMovementMap(name, mapImage);
+        return "redirect:/movement/maps/" + id;
+        /* return "redirect:/maps/"+id;*/
     }
 
     @GetMapping("/{id}")
     //@GetMapping("/maps/{id}")
-    public String getMovementMap (@PathVariable String id, Model model) {
-        MovementMap movementMap= movementMapService.getMovementMap(id);
-        model.addAttribute("name",movementMap.getName());
-        model.addAttribute("mapImage", Base64.getEncoder().encodeToString(movementMap.getMapImage().getData()));
+    public String getMovementMap(@PathVariable String id, Model model) throws IOException{
+        MovementMap movementMap = movementMapService.getMovementMap(id);
+        model.addAttribute("name", movementMap.getName());
+        model.addAttribute("mapImage", Base64.getEncoder().encodeToString(pgm2jpg(movementMap.getMapImage().getData())));
         return "movementMap";
     }
 
@@ -38,5 +44,24 @@ public class MovementMapController {
     @GetMapping("/upload")
     public String uploadMovementMap(Model model) {
         return "uploadMovementMap";
+    }
+
+
+    private static byte[] pgm2jpg(byte[] bytes) throws IOException {
+        int[][] pixels = PGMIO.read(bytes);
+
+        BufferedImage image = new BufferedImage(pixels.length, pixels[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < pixels.length; y++) {
+            for (int x = 0; x < pixels[0].length; x++) {
+                int value = pixels[y][x] << 16 | pixels[y][x] << 8 | pixels[y][x];
+                image.setRGB(x, y, value);
+            }
+        }
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write( image, "jpg", baos );
+        baos.flush();
+        byte[] imageInByte = baos.toByteArray();
+        baos.close();
+        return imageInByte;
     }
 }
