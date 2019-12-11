753eaa4fcfad22e99143aa8522e5f863b71939bf
Mariusz929 <mariusz.bezulski@gmail.com>
2019-12-11 23:33:30 +0100

    fixed url in robots controller

--- raptorsRobotsApp/src/main/java/pl/raptors/raptorsRobotsApp/controller/robots/RobotController.java
+++ raptorsRobotsApp/src/main/java/pl/raptors/raptorsRobotsApp/controller/robots/RobotController.java
@@ -9,7 +9,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/robots/robots")
+@RequestMapping("/robots")
 public class RobotController {
 
     @Autowired
