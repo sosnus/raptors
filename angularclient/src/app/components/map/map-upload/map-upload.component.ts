e34cb20f638d72308f0d8dfd88fddd6db7694d78
ita-11 <ita-11@sr.dmcs.pl>
2019-12-21 23:19:25 +0100

    Map upload validation

--- angularclient/src/app/components/map/map-upload/map-upload.component.ts
+++ angularclient/src/app/components/map/map-upload/map-upload.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { InputFileComponent } from 'ngx-input-file';
-import { MapService } from '../../../services/map.service';
-import { Map } from '../../../model/Map';
+import {Component, OnInit} from '@angular/core';
+import {ViewChild} from '@angular/core';
+import {InputFileComponent} from 'ngx-input-file';
+import {MapService} from '../../../services/map.service';
+import {Map} from '../../../model/Map';
 
 @Component({
   selector: 'app-map-upload',
@@ -14,6 +14,8 @@ export class MapUploadComponent implements OnInit {
   mapSelected = false;
   yamlSelected = false;
   mapName: string = '';
+  success = false;
+  error = false;
 
   constructor(private mapService: MapService) {
 
@@ -29,7 +31,16 @@ export class MapUploadComponent implements OnInit {
   @ViewChild('yamlInput', {static: false})
   private yamlInput: InputFileComponent;
 
-  submitFiles() {
-   this.mapService.save(this.mapName, this.mapInputComponent.files[0].file,this.yamlInput.files[0].file).subscribe(result => console.log(result));
+  submitFiles(): void {
+    this.mapService.save(this.mapName, this.mapInputComponent.files[0].file, this.yamlInput.files[0].file).subscribe(
+      data => this.success = true,
+      error => {
+        console.log(error);
+        this.error = true
+      });
+  }
+
+  checkForm(){
+    return (!this.mapSelected)||(!this.yamlSelected)||(this.mapName.length<1);
   }
 }
