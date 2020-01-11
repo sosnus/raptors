9d3f3ec672274d52a1981c877d4c2c686fa53149
ita-11 <ita-11@sr.dmcs.pl>
2020-01-11 23:54:27 +0100

    Form modal for data input

---
+++ angularclient/src/app/components/adminpanel/form-modal-template/form-modal-template.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-form-modal-template',
+  templateUrl: './form-modal-template.component.html',
+  styleUrls: ['./form-modal-template.component.css']
+})
+export class FormModalTemplateComponent implements OnInit {
+
+  @Input() modalName: string;
+  @Input() disableSubmit: boolean;
+  @Output() onModalClose = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  modalClose(){
+    this.onModalClose.emit();
+  }
+
+}
