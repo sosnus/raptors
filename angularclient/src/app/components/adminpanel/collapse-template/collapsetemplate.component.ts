2c0ac969f99d43be1af62e92b60a4b81f70b29f6
ita-11 <ita-11@sr.dmcs.pl>
2020-01-11 23:54:04 +0100

    Collapse template for forms

---
+++ angularclient/src/app/components/adminpanel/collapse-template/collapsetemplate.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-collapsetemplate',
+  templateUrl: './collapsetemplate.component.html',
+  styleUrls: ['./collapsetemplate.component.css']
+})
+export class CollapsetemplateComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() templateID: string;
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
