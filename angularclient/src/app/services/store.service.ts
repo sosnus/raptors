20682970c76d2bef4f94017e443f492ec6e83a5b
Marek <aim95@wp.pl>
2020-01-29 22:36:53 +0100

    Added orientation to stand

--- angularclient/src/app/services/store.service.ts
+++ angularclient/src/app/services/store.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { Map } from '../model/Map';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Map} from '../model/Map';
+import {Orientation} from "../model/Stand/Orientation";
 
 @Injectable({
   providedIn: 'root'
@@ -12,27 +13,27 @@ export class StoreService {
   public robotIDlist: any;
   public readonly robotIP: string;
 
-  public mapResolution=0.01;
-  public imageResolution=1984;
+  public mapResolution = 0.01;
+  public imageResolution = 1984;
   public mapID = '5e19f1fa9b1eab79e9a58e08';
 
-/*  public robotsObjects = [
-    {
-      id: '5dfb452fd9068433d5983610',
-      robotIP: '10.21.129.22',
-      available: true
-    },
-    {
-      id: '5dfaa9b1501c23010c910a34',
-      robotIP: '10.21.129.23',
-      available: true
-    },
-    {
-      id: '2',
-      robotIP: '10.21.128.10',
-      available: true
-    }
-  ];*/
+  /*  public robotsObjects = [
+      {
+        id: '5dfb452fd9068433d5983610',
+        robotIP: '10.21.129.22',
+        available: true
+      },
+      {
+        id: '5dfaa9b1501c23010c910a34',
+        robotIP: '10.21.129.23',
+        available: true
+      },
+      {
+        id: '2',
+        robotIP: '10.21.128.10',
+        available: true
+      }
+    ];*/
   constructor(private http: HttpClient) {
     this.robotIDlist = 'http://localhost:8080/robots/allById'
     this.robotIP = 'http://localhost:8080/robots/';
@@ -46,10 +47,43 @@ export class StoreService {
 
   public getRobotIP(id: string): Observable<any> {
     const headers = {'Authorization': 'Basic ' + sessionStorage.getItem('token')};
-    return this.http.get(this.robotIP + id + '/ip/',{headers: headers, responseType: 'text'})
+    return this.http.get(this.robotIP + id + '/ip/', {headers: headers, responseType: 'text'})
   }
 
-/*  public getRobotIPfromList(id: string): Observable<any> {
-    return this.http.get(this.robotIP + id + '/ip/' + ... ,{responseType: 'text'})
-  }*/
+  /*  public getRobotIPfromList(id: string): Observable<any> {
+      return this.http.get(this.robotIP + id + '/ip/' + ... ,{responseType: 'text'})
+    }*/
+}
+
+export function quaternionFromAxisAngle(axis, angle): Orientation {
+  // Q = [cos(angle / 2), v * sin(angle / 2)]
+  const halfAngle = angle * 0.5;
+
+  const xAxis = axis[0];
+  const yAxis = axis[1];
+  const zAxis = axis[2];
+
+  const sin = Math.sin(halfAngle);
+  const cos = Math.cos(halfAngle);
+
+  const sin_norm = sin / Math.sqrt(xAxis * xAxis + yAxis * yAxis + zAxis * zAxis);
+
+  return new Orientation(cos, xAxis * sin_norm, yAxis * sin_norm, zAxis * sin_norm);
+}
+
+export function axisAngleFromQuaternion(orientation: Orientation): number {
+  // Q = [cos(angle / 2), v * sin(angle / 2)]
+  const angle = Math.acos(orientation.x) * 2;
+  //TODO()
+  return angle;
+}
+
+export function getCentroid(points) {
+  let centerX = 0;
+  let centerY = 0;
+  points.map(point => {
+    centerX += point.lat;
+    centerY += point.lng;
+  });
+  return [centerX / points.length, centerY / points.length]
 }
---
+++ angularclient/src/lib/leaflet-circle-to-polygon/README.md
@@ -0,0 +1,5 @@
+[![License](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT) [![npm version](https://badge.fury.io/js/leaflet-circle-to-polygon.svg)](https://badge.fury.io/js/leaflet-circle-to-polygon)
+Leaflet.Circle.toPolygon
+========================
+
+Method for interpolating L.Circle into a set of points, for example for use in GeoJSON
