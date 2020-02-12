e4de3f6e90ea5f52e7feed5a5f7824bf29040dd3
kdzierskidawid <dawid1kedzierski@gmail.com>
2020-02-12 17:00:16 +0100

    Add selected robot to Robot Task

--- angularclient/src/app/components/taskpanel/taskpanel-details/taskpanel-details.component.ts
+++ angularclient/src/app/components/taskpanel/taskpanel-details/taskpanel-details.component.ts
@@ -5,6 +5,8 @@ import {Polygon} from "../../../model/MapAreas/Polygons/Polygon";
 import {RobotTaskService} from "../../../services/robotTask.service";
 import {ToastrService} from "ngx-toastr";
 import {StoreService} from "../../../services/store.service";
+import {RobotService} from "../../../services/robot.service";
+import {AreaType} from "../../../model/type/AreaType";
 
 @Component({
   selector: 'app-taskpanel-details',
@@ -16,27 +18,63 @@ export class TaskpanelDetailsComponent implements OnInit {
   @Input()
   task: RobotTask;
 
-  robotTasks: RobotTask[] = [];
+  robots: Robot[] = [];
+  robot = new Robot(null, null, null, null, null, null, null, null, null);
+  robotTasks: RobotTask[]=[];
+  selectedRobot: string;
 
-  constructor(private robotTaskService: RobotTaskService, private toast: ToastrService, private storeService: StoreService) {
+  constructor(private robotService: RobotService, private robotTaskService: RobotTaskService,
+              private toastr: ToastrService) {
 
   }
 
   ngOnInit() {
+    this.robotService.getRobots().subscribe(robots=>{
+      this.robots=robots;
+      console.log("Pobrana lista robotów: " + this.robots);
+    });
+
+    this.getTasks();
+  }
+
+
+  getTasks() {
+    this.robotTaskService.getRobotTasks().subscribe(
+      data => this.robotTasks = data
+    )
+  }
+
+  selectRobot(id: string) {
+    console.log(id);
+    this.selectedRobot = id;
+    this.createOrUpdate();
+  }
+
+  createOrUpdate() {
+    this.robotService.getRobot(this.selectedRobot).subscribe(robot=>{
+      this.task.robot=robot;
+      this.robotTaskService.save(this.task).subscribe(
+        result => {
+          if (this.typeExists(this.task.id)) {
+            this.robotTasks[this.robotTasks.findIndex(item => item.id == result.id)] = result;
+          } else {
+            this.robotTasks.push(result)
+          }
+          //this.task = new AreaType(null, null);
+          this.toastr.success("Dodano robota pomyślnie");
+          console.log(result);
+        },
+        error => {
+          this.toastr.error("Wystąpił bład podczas dodawania");
+        }
+      )
+    });
+
 
   }
 
-  delete() {
-    this.robotTaskService.delete(this.task).subscribe(
-      result => {
-        this.storeService.robotTaskList = this.storeService.robotTaskList.filter(item => item !== this.task);
-        this.toast.success('Anulowano zadanie')
-      },
-      error => {
-        this.toast.error('Wystąpił błąd podczas operacji')
-
-      }
-    )
+  typeExists(id: string) {
+    return this.robotTasks.some(item => item.id == id);
   }
 
 }
