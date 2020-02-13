26a9047e9ec4aeaff85f9c8a777b5d5b4352d739
kdzierskidawid <dawid1kedzierski@gmail.com>
2020-02-13 22:14:04 +0100

    Filter robot statuses by Marek

--- angularclient/src/app/components/taskpanel/taskpanel-details/taskpanel-details.component.ts
+++ angularclient/src/app/components/taskpanel/taskpanel-details/taskpanel-details.component.ts
@@ -5,6 +5,8 @@ import {RobotTaskService} from "../../../services/robotTask.service";
 import {ToastrService} from "ngx-toastr";
 import {RobotService} from "../../../services/robot.service";
 import {AuthService} from "../../../services/auth.service";
+import {RobotStatusService} from "../../../services/type/robot-status.service";
+import {RobotStatus} from "../../../model/Robots/RobotStatus";
 
 @Component({
   selector: 'app-taskpanel-details',
@@ -17,24 +19,33 @@ export class TaskpanelDetailsComponent implements OnInit {
   task: RobotTask;
 
   robots: Robot[] = [];
+  //freeRobots: Robot[] = [];
   robot = new Robot(null, null, null, null, null, null, null, null, null);
   robotTasks: RobotTask[]=[];
   selectedRobot: string;
+  robotStatusFree: RobotStatus = new RobotStatus(null);
 
   constructor(private robotService: RobotService, private robotTaskService: RobotTaskService,private authService: AuthService,
-              private toastr: ToastrService) {
+              private robotStatusService: RobotStatusService, private toastr: ToastrService) {
 
   }
 
   ngOnInit() {
-    this.robotService.getRobots().subscribe(robots=>{
-      this.robots=robots;
-      console.log("Pobrana lista robotów: " + this.robots);
-    });
 
     this.getTasks();
-  }
+    this.robotService.getAll().subscribe(robots=>{
+      this.robots=robots;
+      this.robotStatusService.getAll().subscribe(robotStatus=>{
 
+        robotStatus.forEach(freeStatus=>{
+          if(freeStatus.name==="free"){
+            this.robotStatusFree = freeStatus;
+            this.robots = this.robots.filter(robot=> robot.status.some(state=>state.id=== this.robotStatusFree.id));
+          }
+        });
+      });
+    });
+  }
 
   getTasks() {
     this.robotTaskService.getRobotTasks().subscribe(
@@ -49,17 +60,19 @@ export class TaskpanelDetailsComponent implements OnInit {
   }
 
   createOrUpdate() {
-    this.robotService.getRobot(this.selectedRobot).subscribe(robot=>{
+    this.robotService.getByID(this.selectedRobot).subscribe(robot=>{
       this.task.robot=robot;
+      // uaktualnij status zadania z waiting na zajęte
+      this.task.status="on going";
       // pobierz tylko roboty, które mają status free
+
       // przypisz mu, że juz nie ma zadania, czyli, gdzie robot.status.incluses"free"
 
       // uaktualnij status robota z free na zajęty
-      // uaktualnij status zadania z waiting na zajęte
       //robot.status.includes();
       this.robotTaskService.save(this.task).subscribe(
         result => {
-          if (this.typeExists(this.task.id)) {
+          if (this.taskExists(this.task.id)) {
             this.robotTasks[this.robotTasks.findIndex(item => item.id == result.id)] = result;
           } else {
             this.robotTasks.push(result)
@@ -74,7 +87,7 @@ export class TaskpanelDetailsComponent implements OnInit {
     });
   }
 
-  typeExists(id: string) {
+  taskExists(id: string) {
     return this.robotTasks.some(item => item.id == id);
   }
 
@@ -87,7 +100,7 @@ export class TaskpanelDetailsComponent implements OnInit {
 
   checkIsTaskFree(){
     if(this.task.status === "waiting"){
-      console.log("Status: "  + this.task.status)
+      //console.log("Status: "  + this.task.status)
       return true;
     }
     return null;
@@ -98,6 +111,10 @@ export class TaskpanelDetailsComponent implements OnInit {
       return true;
     }
     return null;
+  }
+
+  checkRobotStatus(robots: Robot[]){
+    //if(robots.)
   }
 
 }
