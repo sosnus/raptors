588fe7600aab0caf335f47a159519d56c54930f3
sosnus <sosnus15@gmail.com>
2020-05-26 02:41:35 +0200

    rm some Angularclint name placeholder

--- ra-front/e2e/src/app.e2e-spec.ts
+++ ra-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angularclient app is running!');
+    expect(page.getTitleText()).toEqual('ra-frontend (prev: angularclient) app is running!');
   });
 
   afterEach(async () => {
--- ra-front/src/app/app.component.ts
+++ ra-front/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {AuthService} from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularclient';
+  title = 'ra-frontend';
 
   constructor(public authService: AuthService) { }
 }
