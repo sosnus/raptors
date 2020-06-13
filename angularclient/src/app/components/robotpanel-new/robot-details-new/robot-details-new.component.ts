962168232660621ce593272e52ba1f5f3a722b12
ZN <37081480+shreq@users.noreply.github.com>
2020-06-13 12:14:02 +0200

    [21] position & orientation formatting (#20)

--- angularclient/src/app/components/robotpanel-new/robot-details-new/robot-details-new.component.ts
+++ angularclient/src/app/components/robotpanel-new/robot-details-new/robot-details-new.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import {timer} from 'rxjs';
 import {Robot} from "../../../model/Robots/Robot";
+import { Orientation } from 'src/app/model/Stand/Orientation';
 
 @Component({
   selector: 'app-robot-details-new',
@@ -23,6 +24,33 @@ export class RobotDetailsComponentNew implements OnInit {
     timer(60000, 60000).subscribe(x => {
       this.refreshEvent.emit();
     })
+  }
+
+  orientationToString(orientation: Orientation) {
+    const o = this.quaternionToEuler(orientation);
+
+    return "yaw " + o[0].toFixed(2) + "° : pitch " + o[1].toFixed(2) + "° : roll " + o[2].toFixed(2) + "°";
+  }
+
+  private quaternionToEuler(quaternion: Orientation) {
+    var t0 = +2.0 * (quaternion.w * quaternion.x + quaternion.y * quaternion.z);
+    var t1 = +1.0 - 2.0 * (quaternion.x * quaternion.x + quaternion.y * quaternion.y);
+    const roll = this.radianToDegree(Math.atan2(t0, t1));
+
+    var t2 = +2.0 * (quaternion.x * quaternion.y - quaternion.z * quaternion.x);
+    t2 = t2 > +1.0 ? +1.0 : t2;
+    t2 = t2 < -1.0 ? -1.0 : t2;
+    const pitch = this.radianToDegree(Math.asin(t2));
+
+    var t3 = +2.0 * (quaternion.w * quaternion.z + quaternion.x * quaternion.y);
+    var t4 = +1.0 - 2.0 * (quaternion.y * quaternion.y + quaternion.z * quaternion.z);
+    const yaw = this.radianToDegree(Math.atan2(t3, t4));
+
+    return [yaw, pitch, roll];
+  }
+
+  private radianToDegree(radian: number) {
+    return radian * 57.2957795131;
   }
 
   constructor() { }
