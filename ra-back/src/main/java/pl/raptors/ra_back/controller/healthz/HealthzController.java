2df10a86780c4c847705eb55459bef80df90b266
Sosnus <sosnus15@gmail.com>
2020-07-01 03:55:52 +0200

    up ra-back to 1.8.6

--- ra-back/src/main/java/pl/raptors/ra_back/controller/healthz/HealthzController.java
+++ ra-back/src/main/java/pl/raptors/ra_back/controller/healthz/HealthzController.java
@@ -24,7 +24,7 @@ public class HealthzController {
     @Autowired
     private HealthzService healthzService;
 
-    private static final String version = "1.8.5-manually"; //HealthzController.getVersion();
+    private static final String version = "1.8.6-manually-test-devops"; //HealthzController.getVersion();
 // private static final String version = HealthzController.getVersion();
 
     // private static String getVersion() {
