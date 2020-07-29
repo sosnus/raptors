447eb2636fe295ba8c468d3a20b8b192a56effc9
leherguargin <maciejleszczynski1997@gmail.com>
2020-07-29 14:30:27 +0200

    nieprzetestowane enviroments

--- ra-front/src/environments/environment.local.ts
+++ ra-front/src/environments/environment.local.ts
@@ -4,4 +4,5 @@ export const environment = {
   refreshRate: 5000,
   baseURL: "http://localhost:8080",
   barrierURL: "https://helike-ra-barrier-sosnus-develop.azurewebsites.net",
+  enviromentStatus: "environment.local.ts",
 };
--- ra-front/src/environments/environment.ts
+++ ra-front/src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   refreshRate: 5000,
   baseURL: "http://localhost:8080",
   barrierURL: "https://helike-ra-barrier-sosnus-develop.azurewebsites.net",
+  enviromentStatus: "environment.ts",
 };
 
 /*
